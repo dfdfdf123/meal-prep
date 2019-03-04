a05a02375f73b75f31e297ce2fb920e797221917
johnromanoff <johnromanoff@yandex.ru>
2019-03-04 16:34:45 +0300

    create navigation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <v-app>
+    <app-navigation></app-navigation>
     <v-content transition="slide-x-transition">
-      <router-view></router-view>  
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import AppNavigation from "@/components/AppNavigation";
 export default {
-  name: 'App',
-}
+  name: "App",
+  components: {
+    AppNavigation
+  }
+};
 </script>
