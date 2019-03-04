2770df28334e521add426d6b07f6b7c34744fff7
johnromanoff <johnromanoff@yandex.ru>
2019-03-04 22:44:37 +0300

    added firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyDUxD1omVGl8HAIhM6oTY7Jt6fJMX6U1aw",
+  authDomain: "meal-prep-95c0c.firebaseapp.com",
+  databaseURL: "https://meal-prep-95c0c.firebaseio.com",
+  projectId: "meal-prep-95c0c",
+  storageBucket: "meal-prep-95c0c.appspot.com",
+  messagingSenderId: "456014988918"
+};
+firebase.initializeApp(config);
